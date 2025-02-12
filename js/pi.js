b52722709749d45e19f2c7643f4c5e7a4cf19a4f
Clodoaldo <cas_maquinas@hotmail.com>
2025-02-12 01:04:01 -0300

    Adição da mudança da cor do icon whatsapp e ajuste de tela

--- js/pi.js
+++ js/pi.js
@@ -1,10 +1,27 @@
-const dropdown = document.querySelector('.dropdown')
-const dropdownContent = document.querySelector('.dropdown-content')
+/**Inicio da programação menu dropdown */
+
+const dropdown = document.querySelector('.dropdown');
+const dropdownContent = document.querySelector('.dropdown-content');
 
 dropdown.addEventListener('mouseover', () => {
     dropdownContent.classList.add('show')
-}) 
+}); 
 
 dropdown.addEventListener('mouseout', () => {
     dropdownContent.classList.remove('show')
-}) 
+}); 
+
+/**Fim para programação menu dropdown */
+
+/**Inicio da mudança da cor do icon whatsapp */
+
+document.addEventListener("scroll", function() {
+    const icon = document.getElementById("icon");
+    if((window.innerHeight+window.scrollY) >= document.body.offsetHeight) {
+        icon.style.color = "white";
+    }else{
+        icon.style.color = "black";
+    }
+});
+
+/**Fim da mudança da cor do whatsapp ao fazer o scroll até o fim da página */
